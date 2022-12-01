ce4ce41395d4ed51a7474b1a566ea72c45a64af1
Angel Travieso <angeltraviesoc@gmail.com>
2022-12-01 10:19:58 -0400

    reproduciendo sonidos en frances

--- app/src/main/java/com/example/frenchteacherapp/MainActivity.java
+++ app/src/main/java/com/example/frenchteacherapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.frenchteacherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // decir el color en frances
+    public void sayTheColor(View view) {
+
+        /*
+            Diciendo que la vista que se pasa como argumento
+            es de tipo Button
+         */
+        Button clickedBtn = (Button) view; // casting
+
+        MediaPlayer mp = MediaPlayer.create(
+                this,
+                getResources().getIdentifier(
+                        clickedBtn.getTag().toString(),
+                        "raw",
+                        getPackageName()
+                )
+        );
+
+        // reproducir sonidos
+        mp.start();
+
+    }
+
 }
